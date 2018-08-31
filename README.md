77c7b35188428f6c89c68edec7629db6dbc19608
Kyle Krull <kkrull@8thlight.com>
2018-08-30 20:36:27 -0500

    Initial templates for JavaScript testing

---
+++ README.md
@@ -0,0 +1,4 @@
+# Live Templates
+
+Why?  Because I'm sick of recreating the same, old Live Templates in JetBrains IDEs every time they upgrade (which is frequent) or every time I move to a new computer (less frequent).
+
---
+++ javascript-testing.js
@@ -0,0 +1,29 @@
+//Scopes: JavaScript and TypeScript -- everywhere. It's good on everything except french fries. 
+
+//afe
+afterEach(() => {
+  $END$
+});
+
+
+//bfe
+beforeEach(() => {
+  $END$
+});
+
+
+//descr
+describe('$NAME$', () => {
+  $END$
+});
+
+
+//it
+it('$NAME$', () => {
+  $END$
+});
+
+
+//pend
+it('$NAME$');
+
